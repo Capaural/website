26498a40a1cc1050505779de15745cf1dc769cc3
dusartvict <dusartvict@eisti.eu>
2020-11-13 17:30:28 +0100

    Ajout du modal (qui marche pas ;)) Ajout du header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PhotoInteractiveComponent } from './photo-interactive/photo-interactive.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhotoInteractiveComponent
+    PhotoInteractiveComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
