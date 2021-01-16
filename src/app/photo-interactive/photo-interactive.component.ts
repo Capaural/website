89fa3805bed95aed58177b71fb493994fc484503
dusartvict <dusartvict@eisti.eu>
2021-01-16 22:21:04 +0100

    Close #1 Ouverture de la modal en cliquant sur la carte

--- src/app/photo-interactive/photo-interactive.component.ts
+++ src/app/photo-interactive/photo-interactive.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-photo-interactive',
@@ -7,37 +8,19 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./photo-interactive.component.scss']
 })
 export class PhotoInteractiveComponent implements OnInit {
-  title = 'appBootstrap';
+  @ViewChild('presentationModal')
+  private presModal: TemplateRef<any>;
 
-  closeResult: string;
-
+  nom: string;
+  
   constructor(private modalService: NgbModal) {}
-
+  
   ngOnInit(): void {}
-
+  
   showInfos(event) {
     var target = event.target || event.srcElement || event.currentTarget;
     var idAttr = target.attributes.title.value; 
-    alert(idAttr);
-  }
-
-  open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-      console.log(this.closeResult);
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-      console.log(this.closeResult);
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
+    this.nom = idAttr;
+    this.modalService.open(this.presModal, {ariaLabelledBy: 'modal-basic-title'});
   }
 }
