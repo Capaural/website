377418a18f0f2c39d2279b4dcb1ee851083d90f5
dusartvict <dusartvict@eisti.eu>
2021-01-19 01:50:33 +0100

    Remove everything

--- src/app/models/Member.model.ts
+++
@@ -1,5 +0,0 @@
-
-export class Member {
-    photo: string;
-    constructor(public name: string, public lastName: string, public alias: string, public role: string, public description: string) {}
-}
