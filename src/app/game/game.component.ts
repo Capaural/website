6238ff21d97dc93449f25c25930b7441d3a3ec21
dusartvict <dusartvict@eisti.eu>
2021-03-09 01:41:32 +0100

    Test d'integration d'une version alpha++ du jeu

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+  gameInstance: any;
+  progress = 0;
+  isReady = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    const loader = (window as any).UnityLoader;
+
+    this.gameInstance = loader.instantiate(
+    'gameContainer', 
+    '/assets/build/Build/WebGL_build.loader.js', {
+    onProgress: (gameInstance: any, progress: number) => {
+        this.progress = progress;
+        if (progress === 1) {
+          this.isReady = true;
+        }
+      }
+    });
   }
 
 }
