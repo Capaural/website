65a002f921066444cb499b078c4adf6ba13c10c3
dusartvict <dusartvict@eisti.eu>
2021-03-14 22:24:02 +0100

    Close #19 Ajout de la vidéo de pres changement du lien discord vers le serveur

---
+++ src/app/video-youtube/video-youtube.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-youtube',
+  templateUrl: './video-youtube.component.html',
+  styleUrls: ['./video-youtube.component.scss']
+})
+export class VideoYoutubeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
