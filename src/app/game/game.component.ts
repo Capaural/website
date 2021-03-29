ad5985b094985f136f3e67455e91ca7967cd5518
dusartvict <dusartvict@eisti.eu>
2021-03-29 16:23:11 +0200

    Le jeu est ajouté sur le site

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-declare const createUnityInstance: any;
 
 @Component({
   selector: 'app-game',
@@ -8,22 +7,9 @@ declare const createUnityInstance: any;
 })
 export class GameComponent implements OnInit {
 
-  gameInstance: any;
-  progress = 0;
-  isReady = false;
-
   constructor() {}
 
   ngOnInit(): void {
-    createUnityInstance(document.querySelector("#unity-canvas"), {
-      dataUrl: "Build/WebGL build.data.unityweb",
-      frameworkUrl: "Build/WebGL build.framework.js.unityweb",
-      codeUrl: "Build/WebGL build.wasm.unityweb",
-      streamingAssetsUrl: "StreamingAssets",
-      companyName: "ExoCarrot",
-      productName: "Capaural",
-      productVersion: "0.1",
-    },null);
-  }
 
+  }
 }
